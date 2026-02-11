f67b1c1d9d7e4a08d852efdce30b099346e4d00c
AymanChabbaki <aymanchabbaki09@gmail.com>
2026-02-11 18:27:04 +0100

    push

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-XX5EDPBE30"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XX5EDPBE30');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
